58e680d687774c9814a4fb57e2e6345d8ab077c4
Kok Kek <kokkekpek@gmail.com>
2021-04-21 17:53:33 +0300

    Add flag: 64 for IRootTokenChangeOwnerAddress.tokenChangeOwnerAddress().

--- contracts/abstract/extensions/tokenChangeOwnerMessageToRoot/token/TokenChangeOwnerAddressMessageToRoot.sol
+++ contracts/abstract/extensions/tokenChangeOwnerMessageToRoot/token/TokenChangeOwnerAddressMessageToRoot.sol
@@ -8,6 +8,6 @@ abstract contract TokenChangeOwnerAddressMessageToRoot is TokenAddress {
      * Call after change of address of token owner.
      */
     function _onChangeOwnerAddress(address previousOwnerAddress, address ownerAddress) override internal {
-        IRootTokenChangeOwnerAddress(_root).tokenChangeOwnerAddress(_id, previousOwnerAddress, ownerAddress);
+        IRootTokenChangeOwnerAddress(_root).tokenChangeOwnerAddress{value: 0, bounce: true, flag: 64}(_id, previousOwnerAddress, ownerAddress);
     }
 }
