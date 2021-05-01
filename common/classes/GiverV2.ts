4621a684c137a835bbee53f02aec558a895488b7
Kok Kek <kokkekpek@gmail.com>
2021-05-01 09:26:16 +0300

    Rename TonKeys to TonKeysFileReader

--- common/classes/GiverV2.ts
+++ common/classes/GiverV2.ts
@@ -1,11 +1,11 @@
 import {KeyPair, ResultOfProcessMessage} from '@tonclient/core/dist/modules'
 import Contract from './base/Contract'
 import GiverV2Contract from '../contracts/GiverV2/GiverV2'
-import TonKeys from './utils/TonKeys'
+import TonKeysFileReader from './utils/TonKeysFileReader'
 
 export default class GiverV2 extends Contract {
     public constructor(keyFile: string) {
-        const keys: KeyPair = TonKeys.readKeys(keyFile)
+        const keys: KeyPair = TonKeysFileReader.read(keyFile)
         super({
             abi: GiverV2Contract.abi,
             initialData: {},
