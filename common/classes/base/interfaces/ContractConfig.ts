04bae895e2e4b6c504ab24f0d705d6b478ad048e
Kok Kek <kokkekpek@gmail.com>
2021-05-01 06:50:12 +0300

    Fix. Swap config params

--- common/classes/base/interfaces/ContractConfig.ts
+++ common/classes/base/interfaces/ContractConfig.ts
@@ -2,8 +2,8 @@ import {AbiContract, KeyPair} from '@tonclient/core/dist/modules'
 
 export default interface ContractConfig {
     abi: AbiContract | Object
-    initialData?: Object
-    keys?: KeyPair
-    tvc?: string
-    address: string
+    initialData: Object
+    keys: KeyPair
+    tvc: string
+    address?: string
 }
