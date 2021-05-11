8b93a6e4ccb5bc06ad732892d7bde028c27ef2b2
Kok Kek <kokkekpek@gmail.com>
2021-05-11 07:12:49 +0300

    Update paths in make script.

--- scripts/make.ts
+++ scripts/make.ts
@@ -10,8 +10,8 @@ const make: Make = new Make({
         'contracts/tokens/ticket/TicketRoot'
     ],
     wrap: [
-        'common/contracts/SafeMultisigWallet/SafeMultisigWallet',
-        'common/contracts/GiverV2/GiverV2'
+        'library/ton/contracts/SafeMultisigWallet/SafeMultisigWallet',
+        'library/ton/contracts/GiverV2/GiverV2'
     ],
     compiler: '0.42.0',
     linker: '0.3.0'
