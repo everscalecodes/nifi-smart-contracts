453a26a8fcb011e7a7adb4289d7cdf584d1755bc
Kok Kek <kokkekpek@gmail.com>
2021-05-19 15:01:41 +0300

    Fix comment.

--- contracts/abstract/extensions/tokenChangeOwnerMessageToRoot/token/TokenChangeOwnerAddressMessageToRoot.sol
+++ contracts/abstract/extensions/tokenChangeOwnerMessageToRoot/token/TokenChangeOwnerAddressMessageToRoot.sol
@@ -6,8 +6,8 @@ import "../../../TokenAddress.sol";
 abstract contract TokenChangeOwnerAddressMessageToRoot is TokenAddress {
     /**
      * Call after change of address of token owner.
-     * previousOwner ... Previous public key of token owner.
-     * owner ........... New public key of token owner.
+     * previousOwner ... Previous address of token owner.
+     * owner ........... New address of token owner.
      */
     function _onChangeOwner(address previousOwner, address owner) override internal {
         IRootTokenChangeOwnerAddress(_root).tokenChangeOwner{value: 0, bounce: true, flag: 64}(
